2b00ec03192df163b25267613446ee3e8d166258
onevcat <onevcat@gmail.com>
2013-07-16 22:55:26 +0900

    Init

---
+++ VVDocumenter-Xcode/VVDocumenter.m
@@ -0,0 +1,15 @@
+//
+//  VVPluginDemo.m
+//  VVDocumenter-Xcode
+//
+//  Created by 王 巍 on 13-7-16.
+//  Copyright (c) 2013年 OneV's Den. All rights reserved.
+//
+
+#import "VVDocumenter.h"
+
+@implementation VVDocumenter
++(void)pluginDidLoad:(NSBundle *)plugin {
+    NSLog(@"VVDocumenter: Plugin loaded successfully");
+}
+@end
