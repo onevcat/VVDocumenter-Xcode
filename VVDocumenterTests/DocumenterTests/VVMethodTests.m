a7dc6727c0fd5fe100066e20117de871af39b2e4
onevcat <onevcat@gmail.com>
2014-01-30 18:59:12 +0900

    Fix and adjust test to use it widely

--- VVDocumenterTests/DocumenterTests/VVMethodTests.m
+++ VVDocumenterTests/DocumenterTests/VVMethodTests.m
@@ -12,7 +12,7 @@
 #import "VVTestHelper.h"
 
 @interface VVMethodTests : SenTestCase
-@property NSArray *testCases;
+@property NSDictionary *testCaseDic;
 @end
 
 @implementation VVMethodTests
@@ -22,41 +22,44 @@
     [super setUp];
     // Put setup code here; it will be run once, before the first test case.
     NSString *path = [[NSBundle bundleForClass:[self class]] pathForResource:@"VVMethodTestsCode" ofType:@"plist"];
-    self.testCases = [NSArray arrayWithContentsOfFile:path];
+    self.testCaseDic = [NSDictionary dictionaryWithContentsOfFile:path];
+
 }
 
 - (void)tearDown
 {
     // Put teardown code here; it will be run once, after the last test case.
+    self.testCaseDic = nil;
     [super tearDown];
 }
 
 -(void) testMethod
 {
-    [self.testCases enumerateObjectsUsingBlock:^(NSDictionary *testDic, NSUInteger idx, BOOL *stop) {
-        NSString *source = testDic[@"source"];
-        NSString *uniform = testDic[@"uniform"];
-        NSString *result = testDic[@"result"];
-        
-        STAssertNotNil(source, @"Test source code should exist.");
-        STAssertNotNil(uniform, @"Test uniform code should exist.");
-        STAssertNotNil(result, @"Test result code should exist.");
-        
-        STAssertEqualObjects([source vv_stringByConvertingToUniform], uniform, @"Source should be converted to uniform format corrctly.");
-        
-        STAssertEqualObjects([source vv_stringByConvertingToUniform], uniform, @"Source should be converted to uniform format corrctly.");
-        
-        NSString *typeString = @"vv_isObjCMethod";
-        STAssertTrue([VVTestHelper performSyntaxMethod:typeString onString:uniform], @"This uniform code should be Objc");
-        
-        NSArray *otherTypeStrings = [VVTestHelper arrayOfExceptCodeType:typeString];
-        for (NSString *type in otherTypeStrings) {
-            STAssertFalse([VVTestHelper performSyntaxMethod:type onString:uniform], @"This uniform code should not be %@",type);
-        }
-        
-        VVDocumenter *documenter = [[VVDocumenter alloc] initWithCode:uniform];
-        STAssertEqualObjects(result, [documenter document], @"Result should be correct");
-        
+    [self.testCaseDic enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSArray *cases, BOOL *stop) {
+        [cases enumerateObjectsUsingBlock:^(NSDictionary *testDic, NSUInteger idx, BOOL *stop) {
+            NSString *source = testDic[@"source"];
+            NSString *uniform = testDic[@"uniform"];
+            NSString *result = testDic[@"result"];
+            
+            STAssertNotNil(source, @"Test source code should exist.");
+            STAssertNotNil(uniform, @"Test uniform code should exist.");
+            STAssertNotNil(result, @"Test result code should exist.");
+            
+            STAssertEqualObjects([source vv_stringByConvertingToUniform], uniform, @"Source should be converted to uniform format corrctly.");
+            
+            STAssertEqualObjects([source vv_stringByConvertingToUniform], uniform, @"Source should be converted to uniform format corrctly.");
+            
+            STAssertTrue([VVTestHelper performSyntaxMethod:key onString:uniform], @"This uniform code should be Objc");
+            
+            NSArray *otherTypeStrings = [VVTestHelper arrayOfExceptCodeType:key];
+            for (NSString *type in otherTypeStrings) {
+                STAssertFalse([VVTestHelper performSyntaxMethod:type onString:uniform], @"This uniform code should not be %@",type);
+            }
+            
+            VVDocumenter *documenter = [[VVDocumenter alloc] initWithCode:uniform];
+            STAssertEqualObjects(result, [documenter document], @"Result should be correct");
+            
+        }];
     }];
 }
 
