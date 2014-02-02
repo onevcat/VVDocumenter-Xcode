2fcf4072b3496521c95301d93e7fa6eb0a783da6
onevcat <onevcat@gmail.com>
2014-02-02 14:31:49 +0900

    Fix void return type for block

--- VVDocumenter-Xcode/Commenter/VVFunctionCommenter.m
+++ VVDocumenter-Xcode/Commenter/VVFunctionCommenter.m
@@ -13,7 +13,8 @@
 -(void) captureReturnType
 {
     NSArray *arr = [self.code componentsSeparatedByString:@"("];
-    if (arr.count > 0 && ![arr[0] vv_matchesPatternRegexPattern:@"^\\s*void\\s*[^*]*\\s*\\w+$"]) {
+    if (arr.count > 0 && (![arr[0] vv_matchesPatternRegexPattern:@"^\\s*void\\s*[^*]*\\s*\\w+$"] &&
+                          ![arr[0] isEqualToString:@"typedef void"])) {
         self.hasReturn = YES;
     }
 }
